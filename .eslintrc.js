196b498cd4e77a74c42e439e0e2c6330e698c3f5
andy luo <andy.luo@sis.ai>
2023-05-01 19:10:44 +0800

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  extends: ['@antfu'],
+  rules: {
+    // vue
+    'vue/component-tags-order': [
+      'error',
+      {
+        order: [
+          'template',
+          'script:not([setup])',
+          'script[setup]',
+          'style:not([scoped])',
+          'style[scoped]',
+        ]
+      }
+    ],
+    // js / ts
+    '@typescript-eslint/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    '@typescript-eslint/space-before-function-paren': ['error', 'always'],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', ignoreRestSiblings: true }],
+    'arrow-parens': ['error', 'always'],
+    'prefer-promise-reject-errors': 'off',
+    'max-len': 'off',
+    'no-underscore-dangle': 'off',
+    'no-shadow': 'off',
+    'no-param-reassign': ['error', { props: false }],
+  }
+}
---
+++ README.md
@@ -0,0 +1,55 @@
+# @andyluo/eslint-config
+
+
+
+Extends [`@antfu/eslint-config`](https://github.com/antfu/eslint-config)
+
+## Usage
+
+### Install
+
+```bash
+pnpm add -D eslint @andyluo/eslint-config
+```
+
+### Config `.eslintrc`
+
+```json
+{
+  "extends": "@andyluo"
+}
+```
+
+### Config `.eslintignore`
+
+```txt
+dist
+node_modules
+```
+
+### Add script for package.json
+
+For example:
+
+```json
+{
+  "scripts": {
+    "lint": "eslint .",
+    "lint:fix": "eslint . --fix"
+  }
+}
+```
+
+### Config VS Code auto fix
+
+Install [VS Code ESLint extension](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint) and create `.vscode/settings.json`
+
+```json
+{
+  "prettier.enable": false,
+  "editor.formatOnSave": false,
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true
+  }
+}
+```
