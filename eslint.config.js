6661c42d4bab7f99d5c3f5682df99fe1c27de56c
andylou0102 <frank87010217@gmail.com>
2024-07-21 11:09:59 +0800

    fix: rules indent

--- eslint.config.js
+++ eslint.config.js
@@ -3,28 +3,28 @@ import antfu from '@antfu/eslint-config'
 export default antfu(
   {
     rules: {
-    // vue
-    'vue/component-tags-order': [
-      'error',
-      {
-        order: [
-          'template',
-          'script:not([setup])',
-          'script[setup]',
-          'style:not([scoped])',
-          'style[scoped]',
-        ]
-      }
-    ],
-    // js / ts
-    '@typescript-eslint/brace-style': ['error', '1tbs', { allowSingleLine: true }],
-    '@typescript-eslint/space-before-function-paren': ['error', 'always'],
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', ignoreRestSiblings: true }],
-    'prefer-promise-reject-errors': 'off',
-    'max-len': 'off',
-    'no-underscore-dangle': 'off',
-    'no-shadow': 'off',
-    'no-param-reassign': ['error', { props: false }],
+      // vue
+      'vue/component-tags-order': [
+        'error',
+        {
+          order: [
+            'template',
+            'script:not([setup])',
+            'script[setup]',
+            'style:not([scoped])',
+            'style[scoped]',
+          ]
+        }
+      ],
+      // js / ts
+      '@typescript-eslint/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      '@typescript-eslint/space-before-function-paren': ['error', 'always'],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', ignoreRestSiblings: true }],
+      'prefer-promise-reject-errors': 'off',
+      'max-len': 'off',
+      'no-underscore-dangle': 'off',
+      'no-shadow': 'off',
+      'no-param-reassign': ['error', { props: false }],
     }
   }
 )
